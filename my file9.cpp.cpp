07f177b343c5284682ec16d827ce6230869abae0
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 09:20:17 -0800

    Add files via upload

---
+++ my file9.cpp.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>//it tells the program to include input output stream library
+using namespace std;// once we write it then we donnot need to write to write std::cout again and again
+// function main begins program execution
+int main()//main point where every C++ program starts
+    {//these curly braces hold information for program that belons to main program
+    int a = 10;//here int a is the data type (integer),a is the name of the variable, and you are assigning it to the value of 10
+    cout << "a = " << a;// it will first print "a=" and then variable
+    
+    
+    return 0;   // tells that program ended successfully
+    } // program ends here
+
