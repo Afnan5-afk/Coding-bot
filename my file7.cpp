e653caa87166d15a11a136ddc6dd6d07df0c37fe
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 05:50:53 -0800

    Add files via upload

---
+++ my file7.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>// loads input/output stream
+using namespace std;// Removes the need of writing std:: prefix
+// function main begins program execution
+int main()// Main line of C++ where every C++ program begins
+    {//{}these curly braces are used to hold functions 
+    cout << "I am in \\ CS department. \n";//Displays data on the screen. n moves it the next line
+    
+    cout << "I am programming enthusiast!";// Displays data on the screen like I am programming enthusiast
+    return 0;   // indicate that program ended successfully
+    } // program ends here
+
