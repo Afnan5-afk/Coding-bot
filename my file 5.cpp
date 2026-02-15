4249ad63d25b8186e5f8eed8b84a34234e15bcaa
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 04:34:26 -0800

    Add files via upload

---
+++ my file 5.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>//A header file and stands for input/output stream used to handle data overflow between your program and outside world
+using namespace std;//It allows you to use cout and cin without typing std::
+// function main begins program execution
+int main()//Main point where program begins 
+    {//{}these curly braces hold data and assign to main program
+    cout << "I am in CS department.";//basically cout shows I am in CS department on screen there is not n at the end of this line so it will contine in one line
+    
+    cout << "I am programming enthusiast!";//same thing but does not go to next line like I am in CS department.I am programming enthusiast
+    return 0;   // shows that program ended successfully
+    } // Here program ends
+
