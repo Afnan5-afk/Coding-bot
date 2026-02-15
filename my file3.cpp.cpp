83edc478ef31c3e4b0fe633c5f1ee4489bc0ee4c
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 03:25:20 -0800

    Add files via upload

---
+++ my file3.cpp.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream> //It stands for input output stream. A sequence of characters or data flowing between your program and a device
+using namespace std;// most standard commads belong to a family called std so we write in once and then we donot need to write std::cout
+//again and again
+// function main begins program execution
+int main()//it is the main function and every C++ program starts from this
+    {// {} Inside these braces belong to main function 
+    cout << "I am in CS department."<<endl;//this represents the standard outstream which is shown on screen and << these are called insertion operator
+    // endl means line ended and ; it is like a full stop
+    
+    cout << "I am programming enthusiast!"; // same thing like above  
+    return 0;   // it tells the operating system that program has finished succesfully
+    } // the end of programming 
+
