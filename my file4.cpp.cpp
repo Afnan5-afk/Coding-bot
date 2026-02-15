8c9d0876e99a3f5ef1afb79a36ed62e2919714ce
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 04:18:28 -0800

    Add files via upload

---
+++ my file4.cpp.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>// A header file and stands for input/output stream 
+using namespace std;// Belongs to a family called std family. It helps us not to write std::cout before every line furthermore it saves our time
+int main()//The starting point the computer runs everything inside{}
+    {
+    cout << "I am in CS department. \n";//It tells the screen go to the next line
+    
+    cout << "I am programming enthusiast!"; //this is next cout line 
+    return 0;   // It means that program has finished successfully
+    } // program ends here
+
