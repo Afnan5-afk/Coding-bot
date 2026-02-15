6476e3d54d4b7cf0320fbff26afb14c41a1f35d1
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 05:42:24 -0800

    Add files via upload

---
+++ my file6.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>//This tells the computer to include the input/output stream library
+using namespace std;//A shortcut that allows us write directly cout without std::
+int main()//this is the main point where program starts
+    {//{}these curly braces hold identities
+    cout << "I am in \r CS department. \n";//Represents the standard output stream and these are called insertion operator and every single instruction must end with semicolon
+    //n lets the statement go to the next line . r here moves the cursor back to the beginning of the current line
+    
+    cout << "I am programming \r enthusiast!";// r here moves the cursor back to the beginning of the current line
+    
+    return 0;   // return 0 means program has fortunately finished
+    } // Here program ends
+
