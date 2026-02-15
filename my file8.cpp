d6d642b7dde3924acbe8e2746ac51be812dc49e9
Afnan5-afk <wazirafnan572@gmail.com>
2026-02-15 09:01:03 -0800

    Add files via upload

---
+++ my file8.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>//It tells the program to include input/output stream
+using namespace std;// it allows you not to write std::cout again and agaim
+
+int main()//main point and every C++ program starts from here
+    {
+    cout << "I am in  CS department. \n";//it tells the cursor to jump to the next line after printing the text
+    
+    cout << "\t I am programming enthusiast!";// \t pushes the text to the right , creating a big space at the start of the line
+    return 0;   //tells the operating system that program finished successfully
+    } //here program ends
+
