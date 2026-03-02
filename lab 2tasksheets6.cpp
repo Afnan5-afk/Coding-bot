21e70f61fedeec5f059dab17ed7fc6548a2874d4
Afnan5-afk <wazirafnan572@gmail.com>
2026-03-02 12:12:09 -0800

    Update lab 2tasksheets6.cpp

--- lab 2tasksheets6.cpp
+++ lab 2tasksheets6.cpp
@@ -1,3 +1,7 @@
+//name: Afnan
+//dept: electrical eng
+//reg no 0659
+//section: B
 #include <iostream>
 using namespace std;
 
@@ -19,3 +23,4 @@ int main()
 
     return 0;//and here program ends 
 }
+
