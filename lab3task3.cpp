62ac1252d5800c5c774758e2dbcde415d9ff38c4
Afnan5-afk <wazirafnan572@gmail.com>
2026-03-02 12:16:09 -0800

    Update lab3task3.cpp

--- lab3task3.cpp
+++ lab3task3.cpp
@@ -1,3 +1,7 @@
+//name: Afnan
+//dept: electrical eng
+//reg no 0659
+//section: B
 #include <iostream>   // For input and output
 using namespace std;
 
@@ -19,3 +23,4 @@ int main() {
 
     return 0;   // Program ends successfully
 }
+
