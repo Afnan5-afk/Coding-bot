ed0290795226c27453ed1f5aa1c17bb3bef7960d
Afnan5-afk <wazirafnan572@gmail.com>
2026-03-08 01:01:12 -0800

    Add files via upload

---
+++ lab 2 files/lab 2,2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//header file
+using namespace std;//used not to write std before every line
+
+int main() {//every C++ program starts from here
+    float pi = 3.14f;   // used used float for values like in decimal form
+
+    cout << "Value of pi: " << pi << endl;//it is showing the value of pi on screen
+
+    return 0;//here the program ends 
+}
