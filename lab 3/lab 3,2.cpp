34fe4b2604179ccfc22988455565ffc22c80c303
Afnan5-afk <wazirafnan572@gmail.com>
2026-03-08 10:10:19 -0700

    Add files via upload

---
+++ lab 3/lab 3,2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int number;
+	cout<<"hello user entter a number : ";
+	cin>>number;
+	if(number>220){
+		cout<<"your number is greather than two twenty"<<endl;
+	}
+	return 0;
+}
